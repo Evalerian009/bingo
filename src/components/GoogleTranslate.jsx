e03f9380bf2c535d98d590786a61f89f38830751
Evalerian009 <emyval009@gmail.com>
2023-12-25 17:39:13 +0100

    commit

--- src/components/GoogleTranslate.jsx
+++ src/components/GoogleTranslate.jsx
@@ -1,6 +1,13 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import GT from './assets/gt.png'
 
 const GoogleTranslate = () => {
+  const [ open, setOpen ] = useState(false)
+
+  const handler = () =>((
+    setOpen(!open)
+  ))
+  
   const googleTranslateElementInit = () => {
     new window.google.translate.TranslateElement(
       {
@@ -21,7 +28,12 @@ const GoogleTranslate = () => {
     }, []);
 
   return (
-    <div id="google_translate_element" className='fixed z-[999999] bottom-[50px] right-[50px] bg-[#036aa153] hover:bg-sky-800 p-2 text-white rounded-lg'></div>
+    <div className='fixed z-[999999] bottom-[50px] right-[20px] md:right-[50px] bg-[#036aa153] hover:bg-sky-800 p-2 text-white rounded-lg flex flex-col items-center'>
+      <button className='w-[50px] h-[50px] md:w-[70px] md:h-[70px]' onClick={handler}>
+        <img src={GT} alt='google-translate' className='w-full h-full' />
+      </button>
+      <div id="google_translate_element" className={`${open ? 'inline-block' : 'hidden'} transition-all duration-150`} />
+    </div>
   )
 }
 
